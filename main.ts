0ba47760e91a71863218ec8a2a9e1aa36dd1b8fe
st19060nk <66375889+st19060nk@users.noreply.github.com>
2021-10-21 12:22:35 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # . # . #
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . #
+        # # # # #
+        # . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # # #
+        . . # . #
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . #
+        # # # # #
+        # . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # # #
+        . . # . #
+        . # . # .
+        # . . # .
+        `)
+})
